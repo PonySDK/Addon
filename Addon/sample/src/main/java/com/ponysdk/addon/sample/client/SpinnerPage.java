d9104e45a31e07d2b46265d20bada284f6c18b56
mathieu.barbier@gmail.com <mathieu.barbier@gmail.com>
2013-08-06 14:01:47 +0200

    Spinner: Fix big max

--- Addon/sample/src/main/java/com/ponysdk/addon/sample/client/SpinnerPage.java
+++ Addon/sample/src/main/java/com/ponysdk/addon/sample/client/SpinnerPage.java
@@ -1,4 +1,3 @@
-
 package com.ponysdk.addon.sample.client;
 
 import com.ponysdk.addon.spinner.server.PSpinner;
@@ -22,92 +21,92 @@ import com.ponysdk.ui.server.form2.formfield.StringTextBoxFormField;
 
 public class SpinnerPage implements IsPWidget, PValueChangeHandler<String> {
 
-    private int row = 0;
+	private int row = 0;
 
-    private final PScrollPanel scroll = new PScrollPanel();
-    private final PFlowPanel flow = new PFlowPanel();
-    private final PFlexTable table = new PFlexTable();
-    private final StringTextBoxFormField valueFormField;
-    private final DoubleTextBoxFormField minFormField;
-    private final DoubleTextBoxFormField maxFormField;
-    private final DoubleTextBoxFormField stepFormField;
-    private final IntegerTextBoxFormField pageFormField;
-    private final IntegerTextBoxFormField formatFormField;
-    private final PSpinner spinner;
+	private final PScrollPanel scroll = new PScrollPanel();
+	private final PFlowPanel flow = new PFlowPanel();
+	private final PFlexTable table = new PFlexTable();
+	private final StringTextBoxFormField valueFormField;
+	private final DoubleTextBoxFormField minFormField;
+	private final DoubleTextBoxFormField maxFormField;
+	private final DoubleTextBoxFormField stepFormField;
+	private final IntegerTextBoxFormField pageFormField;
+	private final IntegerTextBoxFormField formatFormField;
+	private final PSpinner spinner;
 
-    public SpinnerPage() {
+	public SpinnerPage() {
 
-        final PSpinner.Options options = new PSpinner.Options();
-        options.withMin(0d).withMax(100d).withStep(0.01d).withPage(10).withNumberFormat(2);
+		final PSpinner.Options options = new PSpinner.Options();
+		options.withMin(0d).withMax(100d).withStep(0.01d).withPage(10)
+				.withNumberFormat(2);
 
-        valueFormField = new StringTextBoxFormField();
-        minFormField = new DoubleTextBoxFormField();
-        maxFormField = new DoubleTextBoxFormField();
-        stepFormField = new DoubleTextBoxFormField();
-        pageFormField = new IntegerTextBoxFormField();
-        formatFormField = new IntegerTextBoxFormField();
+		valueFormField = new StringTextBoxFormField();
+		minFormField = new DoubleTextBoxFormField();
+		maxFormField = new DoubleTextBoxFormField();
+		stepFormField = new DoubleTextBoxFormField();
+		pageFormField = new IntegerTextBoxFormField();
+		formatFormField = new IntegerTextBoxFormField();
 
-        spinner = new PSpinner(options);
-        spinner.addValueChangeHandler(this);
+		spinner = new PSpinner(options);
+		spinner.addValueChangeHandler(this);
 
-        valueFormField.setValue(options.getValue());
-        minFormField.setValue(options.getMin());
-        maxFormField.setValue(options.getMax());
-        stepFormField.setValue(options.getStep());
-        pageFormField.setValue(options.getPage());
-        formatFormField.setValue(options.getDecimals());
+		valueFormField.setValue("");
+		minFormField.setValue(options.getMin());
+		maxFormField.setValue(options.getMax());
+		stepFormField.setValue(options.getStep());
+		pageFormField.setValue(options.getPage());
+		formatFormField.setValue(options.getDecimals());
 
-        addField("Value", valueFormField);
-        addField("Min", minFormField);
-        addField("Max", maxFormField);
-        addField("Step", stepFormField);
-        addField("Page", pageFormField);
-        addField("Decimals", formatFormField);
+		addField("Value", valueFormField);
+		addField("Min", minFormField);
+		addField("Max", maxFormField);
+		addField("Step", stepFormField);
+		addField("Page", pageFormField);
+		addField("Decimals", formatFormField);
 
-        table.setWidget(row++, 0, new PHTML("&nbsp;"));
-        table.setWidget(row++, 0, spinner);
-        table.getFlexCellFormatter().setColSpan(row - 1, 0, 3);
+		table.setWidget(row++, 0, new PHTML("&nbsp;"));
+		table.setWidget(row++, 0, spinner);
+		table.getFlexCellFormatter().setColSpan(row - 1, 0, 3);
 
-        flow.add(table);
+		flow.add(table);
 
-        scroll.setWidget(flow);
-    }
+		scroll.setWidget(flow);
+	}
 
-    private void addField(final String label, final FormField<?> formField) {
-        final PButton update = new PButton("Update");
-        update.addClickHandler(new PClickHandler() {
+	private void addField(final String label, final FormField<?> formField) {
+		final PButton update = new PButton("Update");
+		update.addClickHandler(new PClickHandler() {
 
-            @Override
-            public void onClick(final PClickEvent event) {
-                final PSpinner.Options options = buidOptions();
-                spinner.setOptions(options);
-            }
-        });
-        table.setWidget(row, 0, new PLabel(label));
-        table.setWidget(row, 1, formField.asWidget());
-        table.setWidget(row, 2, update);
-        row++;
-    }
+			@Override
+			public void onClick(final PClickEvent event) {
+				final PSpinner.Options options = buidOptions();
+				spinner.setOptions(options);
+			}
+		});
+		table.setWidget(row, 0, new PLabel(label));
+		table.setWidget(row, 1, formField.asWidget());
+		table.setWidget(row, 2, update);
+		row++;
+	}
 
-    protected Options buidOptions() {
-        final PSpinner.Options options = new PSpinner.Options();
-        options.withMin(minFormField.getValue());
-        options.withMax(maxFormField.getValue());
-        options.withStep(stepFormField.getValue());
-        options.withPage(pageFormField.getValue());
-        options.withNumberFormat(formatFormField.getValue());
-        options.withValue(valueFormField.getValue());
-        return options;
-    }
+	protected Options buidOptions() {
+		final PSpinner.Options options = new PSpinner.Options();
+		options.withMin(minFormField.getValue());
+		options.withMax(maxFormField.getValue());
+		options.withStep(stepFormField.getValue());
+		options.withPage(pageFormField.getValue());
+		options.withNumberFormat(formatFormField.getValue());
+		return options;
+	}
 
-    @Override
-    public PWidget asWidget() {
-        return scroll;
-    }
+	@Override
+	public PWidget asWidget() {
+		return scroll;
+	}
 
-    @Override
-    public void onValueChange(final PValueChangeEvent<String> event) {
-        valueFormField.setValue(event.getValue());
-    }
+	@Override
+	public void onValueChange(final PValueChangeEvent<String> event) {
+		valueFormField.setValue(event.getValue());
+	}
 
 }
--- Addon/spinner/src/main/java/com/ponysdk/addon/spinner/server/PSpinner.java
+++ Addon/spinner/src/main/java/com/ponysdk/addon/spinner/server/PSpinner.java
@@ -1,6 +1,6 @@
-
 package com.ponysdk.addon.spinner.server;
 
+import java.math.BigDecimal;
 import java.util.Collection;
 
 import org.json.JSONException;
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import com.ponysdk.addon.spinner.client.D;
 import com.ponysdk.core.tools.ListenerCollection;
+import com.ponysdk.core.tools.Objects;
 import com.ponysdk.ui.server.basic.HasPValue;
 import com.ponysdk.ui.server.basic.PElement;
 import com.ponysdk.ui.server.basic.event.PHasText;
@@ -19,234 +20,276 @@ import com.ponysdk.ui.server.basic.event.PValueChangeEvent;
 import com.ponysdk.ui.server.basic.event.PValueChangeHandler;
 import com.ponysdk.ui.server.utils.E;
 
-public class PSpinner extends PElement implements PHasText, HasPValue<String>, PValueChangeHandler<String>, PNativeHandler {
+public class PSpinner extends PElement implements PHasText, HasPValue<String>,
+		PValueChangeHandler<String>, PNativeHandler {
 
-    private static Logger log = LoggerFactory.getLogger(PSpinner.class);
+	private static Logger log = LoggerFactory.getLogger(PSpinner.class);
 
-    private final ListenerCollection<PValueChangeHandler<String>> handlers = new ListenerCollection<PValueChangeHandler<String>>();
+	private final ListenerCollection<PValueChangeHandler<String>> handlers = new ListenerCollection<PValueChangeHandler<String>>();
 
-    private double min;
-    private double max;
-    private double step;
-    private int page;
-    private int decimals;
+	private double min;
+	private double max;
+	private double step;
+	private int page;
+	private int decimals;
 
-    private String value;
+	private String value;
 
-    public PSpinner(final Options options) {
-        super("span");
-        bindTerminalFunction("spinner");
-        setOptions(options);
-        addNativeHandler(this);
-    }
+	private boolean enabled = true;
+	private int tabindex;
 
-    public void setOptions(final Options options) {
+	private boolean focused;
 
-        final JSONObject jso = new JSONObject();
+	public PSpinner(final Options options) {
+		super("span");
+		bindTerminalFunction("spinner");
+		setOptions(options);
+		addNativeHandler(this);
+	}
 
-        if (options.min != null) {
-            min = options.min;
-            update(jso, D.MIN, String.valueOf(options.min));
-        }
+	public void setOptions(final Options options) {
 
-        if (options.max != null) {
-            max = options.max;
-            update(jso, D.MAX, String.valueOf(options.max));
-        }
+		final JSONObject jso = new JSONObject();
 
-        if (options.step != null) {
-            step = options.step;
-            update(jso, D.STEP, String.valueOf(options.step));
-        }
+		if (options.min != null) {
+			min = options.min;
+			update(jso, D.MIN, doubleToString(options.min));
+		}
 
-        if (options.page != null) {
-            page = options.page;
-            update(jso, D.PAGE, options.page);
-        }
+		if (options.max != null) {
+			max = options.max;
+			update(jso, D.MAX, doubleToString(max));
+		}
 
-        if (options.decimals != null) {
-            decimals = options.decimals;
-            update(jso, D.DECIMAL, options.decimals);
-        }
+		if (options.step != null) {
+			step = options.step;
+			update(jso, D.STEP, doubleToString(options.step));
+		}
 
-        if (options.value != null) {
-            value = options.value;
-            update(jso, D.TEXT, options.value);
-        }
+		if (options.page != null) {
+			page = options.page;
+			update(jso, D.PAGE, options.page);
+		}
 
-        sendToNative(jso);
-    }
+		if (options.decimals != null) {
+			decimals = options.decimals;
+			update(jso, D.DECIMAL, options.decimals);
+		}
 
-    public void setMin(final double min) {
-        if (this.min == min) return;
+		sendToNative(jso);
+	}
 
-        this.min = min;
-        updateAndSend(new JSONObject(), D.MIN, min);
-    }
+	protected String doubleToString(final double d) {
+		return new BigDecimal(Double.toString(d)).toPlainString();
+	}
 
-    public void setMax(final double max) {
-        if (this.max == max) return;
+	public void setMin(final double min) {
+		if (this.min == min)
+			return;
 
-        this.max = max;
-        updateAndSend(new JSONObject(), D.MAX, max);
-    }
+		this.min = min;
+		updateAndSend(new JSONObject(), D.MIN, doubleToString(min));
+	}
 
-    public void setStep(final double step) {
-        if (this.step == step) return;
+	public void setMax(final double max) {
+		if (this.max == max)
+			return;
 
-        this.step = step;
-        updateAndSend(new JSONObject(), D.STEP, step);
-    }
+		this.max = max;
+		updateAndSend(new JSONObject(), D.MAX, doubleToString(max));
+	}
 
-    public void setPage(final int page) {
-        if (this.page == page) return;
+	public void setStep(final double step) {
+		if (this.step == step)
+			return;
 
-        this.page = page;
-        updateAndSend(new JSONObject(), D.PAGE, page);
-    }
+		this.step = step;
+		updateAndSend(new JSONObject(), D.STEP, doubleToString(step));
+	}
 
-    public void setDecimals(final int decimals) {
-        if (this.decimals == decimals) return;
+	public void setPage(final int page) {
+		if (this.page == page)
+			return;
 
-        this.decimals = decimals;
-        updateAndSend(new JSONObject(), D.DECIMAL, decimals);
-    }
+		this.page = page;
+		updateAndSend(new JSONObject(), D.PAGE, page);
+	}
 
-    @Override
-    public void setValue(final String value) {
-        if (E.quals(this.value, value)) return;
+	public void setDecimals(final int decimals) {
+		if (this.decimals == decimals)
+			return;
 
-        this.value = value;
-        updateAndSend(new JSONObject(), D.TEXT, value);
-    }
+		this.decimals = decimals;
+		updateAndSend(new JSONObject(), D.DECIMAL, decimals);
+	}
 
-    private void update(final JSONObject jso, final String property, final Object v) {
-        try {
-            jso.put(property, v);
-        } catch (final JSONException e) {
-            log.error("Failed to update json object. " + property + "/" + v, e);
-        }
-    }
+	@Override
+	public void setValue(final String value) {
+		if (E.quals(this.value, value))
+			return;
 
-    private void updateAndSend(final JSONObject jso, final String property, final Object v) {
-        update(jso, property, v);
-        sendToNative(jso);
-    }
+		this.value = value;
+		updateAndSend(new JSONObject(), D.TEXT, value);
+	}
 
-    public static class Options {
+	private void update(final JSONObject jso, final String property,
+			final Object v) {
+		try {
+			jso.put(property, v);
+		} catch (final JSONException e) {
+			log.error("Failed to update json object. " + property + "/" + v, e);
+		}
+	}
 
-        protected Double min;
-        protected Double max;
-        protected Double step;
-        protected Integer page;
-        protected Integer decimals;
-        protected String value;
+	private void updateAndSend(final JSONObject jso, final String property,
+			final Object v) {
+		update(jso, property, v);
+		sendToNative(jso);
+	}
 
-        public Options() {}
+	public static class Options {
 
-        public Options withMin(final Double m) {
-            this.min = m;
-            return this;
-        }
+		protected Double min;
+		protected Double max;
+		protected Double step;
+		protected Integer page;
+		protected Integer decimals;
 
-        public Options withMax(final Double m) {
-            this.max = m;
-            return this;
-        }
+		public Options() {
+		}
 
-        public Options withPage(final Integer p) {
-            this.page = p;
-            return this;
-        }
+		public Options withMin(final Double m) {
+			this.min = m;
+			return this;
+		}
 
-        public Options withStep(final Double s) {
-            this.step = s;
-            return this;
-        }
+		public Options withMax(final Double m) {
+			this.max = m;
+			return this;
+		}
 
-        public Options withNumberFormat(final Integer nf) {
-            this.decimals = nf;
-            return this;
-        }
+		public Options withPage(final Integer p) {
+			this.page = p;
+			return this;
+		}
 
-        public Options withValue(final String value) {
-            this.value = value;
-            return this;
-        }
+		public Options withStep(final Double s) {
+			this.step = s;
+			return this;
+		}
 
-        public Double getMin() {
-            return min;
-        }
+		public Options withNumberFormat(final Integer nf) {
+			this.decimals = nf;
+			return this;
+		}
 
-        public Double getMax() {
-            return max;
-        }
+		public Double getMin() {
+			return min;
+		}
 
-        public Double getStep() {
-            return step;
-        }
+		public Double getMax() {
+			return max;
+		}
 
-        public Integer getPage() {
-            return page;
-        }
+		public Double getStep() {
+			return step;
+		}
 
-        public Integer getDecimals() {
-            return decimals;
-        }
+		public Integer getPage() {
+			return page;
+		}
 
-        public String getValue() {
-            return value;
-        }
+		public Integer getDecimals() {
+			return decimals;
+		}
 
-    }
+	}
 
-    @Override
-    public void addValueChangeHandler(final PValueChangeHandler<String> handler) {
-        handlers.add(handler);
-    }
+	@Override
+	public void addValueChangeHandler(final PValueChangeHandler<String> handler) {
+		handlers.add(handler);
+	}
 
-    @Override
-    public void removeValueChangeHandler(final PValueChangeHandler<String> handler) {
-        handlers.remove(handler);
-    }
+	@Override
+	public void removeValueChangeHandler(
+			final PValueChangeHandler<String> handler) {
+		handlers.remove(handler);
+	}
 
-    @Override
-    public Collection<PValueChangeHandler<String>> getValueChangeHandlers() {
-        return handlers;
-    }
+	@Override
+	public Collection<PValueChangeHandler<String>> getValueChangeHandlers() {
+		return handlers;
+	}
 
-    @Override
-    public void onValueChange(final PValueChangeEvent<String> event) {
-        value = event.getValue();
-        for (final PValueChangeHandler<String> handler : handlers) {
-            handler.onValueChange(event);
-        }
-    }
+	@Override
+	public void onValueChange(final PValueChangeEvent<String> event) {
+		value = event.getValue();
+		for (final PValueChangeHandler<String> handler : handlers) {
+			handler.onValueChange(event);
+		}
+	}
 
-    @Override
-    public String getValue() {
-        return value;
-    }
+	@Override
+	public String getValue() {
+		return value;
+	}
 
-    @Override
-    public String getText() {
-        return getValue();
-    }
+	@Override
+	public String getText() {
+		return getValue();
+	}
 
-    @Override
-    public void setText(final String text) {
-        setValue(text);
-    }
+	@Override
+	public void setText(final String text) {
+		setValue(text);
+	}
 
-    @Override
-    public void onNativeEvent(final PNativeEvent event) {
-        try {
-            final JSONObject data = event.getJsonObject();
-            final String value = data.getString("value");
-            final PValueChangeEvent<String> e = new PValueChangeEvent<String>(this, value);
-            onValueChange(e);
-        } catch (final JSONException e) {
-            log.error("Failed to read json object.", e);
-        }
-    }
+	public void setEnabled(final boolean enabled) {
+		if (Objects.equals(this.enabled, enabled))
+			return;
+		this.enabled = enabled;
+
+		updateAndSend(new JSONObject(), D.ENABLED, enabled);
+	}
+
+	public void setTabindex(final int tabindex) {
+		if (this.tabindex == tabindex)
+			return;
+		this.tabindex = tabindex;
+
+		updateAndSend(new JSONObject(), D.TABINDEX, tabindex);
+	}
+
+	public void setFocus(final boolean focused) {
+		if (Objects.equals(this.focused, focused))
+			return;
+		this.focused = focused;
+
+		updateAndSend(new JSONObject(), D.FOCUSED, focused);
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public int getTabindex() {
+		return tabindex;
+	}
+
+	public boolean isFocused() {
+		return focused;
+	}
+
+	@Override
+	public void onNativeEvent(final PNativeEvent event) {
+		try {
+			final JSONObject data = event.getJsonObject();
+			final String value = data.getString("value");
+			final PValueChangeEvent<String> e = new PValueChangeEvent<String>(
+					this, value);
+			onValueChange(e);
+		} catch (final JSONException e) {
+			log.error("Failed to read json object.", e);
+		}
+	}
 }
